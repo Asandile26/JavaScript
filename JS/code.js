4009d7d7e67230e0524092fb634100efb5623669
Asandile <asandilelangeni@gmail.com>
2022-11-07 13:54:45 +0200

    first commit

---
+++ JS/code.js
@@ -0,0 +1,10 @@
+// //document
+// document.write("Hello World");
+// document.write("<br>My Name is Asandile Langeni");
+// document.writeln("<br> Am a developer");
+
+let username = "Asandile";
+let userAge= 27;
+console.log(username);
+console.log(userAge);
+console.log(typeof username);
